4d9974644990c16bc0aecedc7f52b02407cc1f1a
Mattjava <77417435+Mattjava@users.noreply.github.com>
2025-09-04 20:10:12 -0400

    Updated the main structure of the project.
    It should now finally run without issue.

--- Library-Manager/src/main/java/com/example/Library/Manager/LibraryManagerApplication.java
+++ Library-Manager/src/main/java/com/example/Library/Manager/LibraryManagerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryManagerApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(LibraryManagerApplication.class, args);
 	}
 
