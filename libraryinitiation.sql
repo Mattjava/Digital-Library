a7ea97d9b83c3aa634d3eb781c6d42de2b897482
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2025-09-06 23:33:54 -0400

    Update libraryinitiation.sql
    Removed comments from the file. This enables the file to create the essential databases and tables.

--- libraryinitiation.sql
+++ libraryinitiation.sql
@@ -1,21 +1,21 @@
--- create database library;
+ create database library;
 
--- use library;
+ use library;
 
--- create table Book(
--- 	ISBN int not null auto_increment,
---     Title varchar(255) default "Lorem Ipsum",
---     Author varchar(255) default "John Doe",
---     Publisher varchar(255) default "No Publisher",
---     Genre varchar(255) default "None",
---     PageLength int default 100,
---     
---     primary key (ISBN)
--- );
+ create table Book(
+ 	ISBN int not null auto_increment,
+     Title varchar(255) default "Lorem Ipsum",
+     Author varchar(255) default "John Doe",
+     Publisher varchar(255) default "No Publisher",
+     Genre varchar(255) default "None",
+     PageLength int default 100,
+     
+     primary key (ISBN)
+ );
 
--- insert into Book(Title, Author, Publisher, Genre, PageLength) values
--- 	("Frankenstein", "Mary Shelly", "Simon & Schuster", "Horror", 250),
---     ("Don Quixote", "Miguel de Cervantes", "", "Adventure", 500),
---     ("Dictionary", "", "", "", 1000);
+ insert into Book(Title, Author, Publisher, Genre, PageLength) values
+ 	("Frankenstein", "Mary Shelly", "Simon & Schuster", "Horror", 250),
+     ("Don Quixote", "Miguel de Cervantes", "", "Adventure", 500),
+     ("Dictionary", "", "", "", 1000);
     
 select * from Book where Author != "";
